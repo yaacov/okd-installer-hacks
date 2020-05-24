2ef3583a9f13cb79911e591661854d3698263c76
Yaacov Zamir <kobi.zamir@gmail.com>
2020-05-24 09:46:46 +0300

    Update config_dev.sh

--- dev-scripts/config_dev.sh
+++ dev-scripts/config_dev.sh
@@ -9,8 +9,8 @@ export WORKER_VCPU=4
 
 export NUM_MASTERS=3
 export MASTER_MEMORY=65536
-export MASTER_DISK=100
-export MASTER_VCPU=8
+export MASTER_DISK=200
+export MASTER_VCPU=4
 
 # Get pull image from https://cloud.redhat.com/openshift/install#pull-secret.
 # When using registry.svc.ci.openshift.org we need to add the registtry auth to the pull secret.
