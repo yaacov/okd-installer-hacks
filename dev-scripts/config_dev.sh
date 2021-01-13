d6336d902111841e78b32536bf95d33eac7b82d4
Yaacov Zamir <kobi.zamir@gmail.com>
2021-01-13 17:21:33 +0200

    Update config_dev.sh

--- dev-scripts/config_dev.sh
+++ dev-scripts/config_dev.sh
@@ -4,12 +4,12 @@ export VIRSH_DEFAULT_CONNECT_URI=qemu:///system
 
 export NUM_WORKERS=3
 export WORKER_MEMORY=65536
-export WORKER_DISK=200
+export WORKER_DISK=20
 export WORKER_VCPU=4
 
 export NUM_MASTERS=3
 export MASTER_MEMORY=65536
-export MASTER_DISK=200
+export MASTER_DISK=20
 export MASTER_VCPU=4
 
 # export OPENSHIFT_RELEASE_IMAGE='quay.io/openshift-release-dev/ocp-release:4.4.0-rc.10-x86_64'
